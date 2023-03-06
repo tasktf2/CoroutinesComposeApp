c9f9ad23d352b28af63d05f52f13a928764aa893
tasktf2 <task.tf2@gmail.com>
2023-03-07 00:43:12 +0500

    test

--- app/src/main/java/com/example/coroutinescomposeapp/MainActivity.kt
+++ app/src/main/java/com/example/coroutinescomposeapp/MainActivity.kt
@@ -18,7 +18,7 @@ class MainActivity : ComponentActivity() {
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background,
-                ) {
+                ) {// .
                 }
             }
         }
